f1b08869049ea24244f10a0ef21b7f3c3f7ae6b7
Lotfi Jellali <lotfijellali40@gmail.com>
2024-03-10 03:35:28 +0100

    signin controller

--- back-end/src/controllers/user.js
+++ back-end/src/controllers/user.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
+// signup
 exports.signup = async (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
 
@@ -26,3 +28,33 @@ exports.signup = async (req, res, next) => {
     res.status(400).json({ message: "Something went wrong" });
   }
 };
+
+exports.signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const found = await User.findOne({ email });
+    if (!found)
+      return res.status(400).json({ message: "User Email not found" });
+    else {
+      if (found.authenticate(req.body.password)) {
+        const token = jwt.sign({ _id: found._id }, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        const { firstName, lastName, role, fullName } = found;
+
+        res.status(200).json({
+          message: "succssfully signed in",
+          token,
+          user: { firstName, lastName, email, role, fullName },
+        });
+      } else {
+        return res.status(400).json({ message: "This is invalid password" });
+      }
+    }
+  } catch {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+// signin
--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const { signup } = require("../controllers/user");
+const { signup, signin } = require("../controllers/user");
 const router = express.Router();
 
 // Routes
 
-router.post("/signin", (req, res) => {
-  res.send("user");
-});
+router.post("/signin", signin);
 
 router.post("/signup", signup);
 
